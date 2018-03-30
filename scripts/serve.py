176e3a3c1e23e669320edfdca69c4a05d5a51f7e
Nicholas Lourie <nicholasl@allenai.org>
2018-03-29 23:24:48 -0700

    Remove the debug server
    The debug server seems to have issues and not work. My guess is that
    the debug server can't handle the websocket traffic.

--- scripts/serve.py
+++ scripts/serve.py
@@ -9,7 +9,6 @@ import click
 from gevent.wsgi import WSGIServer
 
 import backend
-from backend import settings
 
 
 logger = logging.getLogger(__name__)
@@ -19,20 +18,12 @@ logger = logging.getLogger(__name__)
     context_settings={
         'help_option_names': ['-h', '--help']
     })
-@click.option(
-    '--debug', '-d',
-    is_flag=True,
-    help='Run the server in debug mode.')
-def serve(debug=False):
+def serve():
     """Serve twentyquestions on port 5000."""
+    logger.info('Running prod server on http://127.0.0.1:5000/')
 
-    if debug:
-        logger.info('Running debug server on http://127.0.0.1:5000/')
-        backend.app.run(host='0.0.0.0', debug=True)
-    else:
-        logger.info('Running prod server on http://127.0.0.1:5000/')
-        http_server = WSGIServer(('0.0.0.0', 5000), backend.app)
-        http_server.serve_forever()
+    http_server = WSGIServer(('0.0.0.0', 5000), backend.app)
+    http_server.serve_forever()
 
 
 if __name__ == '__main__':
